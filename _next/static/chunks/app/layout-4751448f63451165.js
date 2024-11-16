d119da7740740e760c093d00b5e4ed8f626301de
Saikowshik007 <58332764+Saikowshik007@users.noreply.github.com>
2024-11-16 00:43:30 +0000

    Deploying to gh-pages from @ Saikowshik007/behavioral-dash@17b702203d028e231d8e8cb51b57d2a47bd318bd 🚀

---
+++ _next/static/chunks/app/layout-4751448f63451165.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{13981:(e,s,a)=>{Promise.resolve().then(a.t.bind(a,30347,23)),Promise.resolve().then(a.t.bind(a,78346,23))},30347:()=>{},78346:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_f4ca69"}}},e=>{var s=s=>e(e.s=s);e.O(0,[838,441,517,358],()=>s(13981)),_N_E=e.O()}]);
